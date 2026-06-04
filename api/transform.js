a85e90d4eee22d23375bdc45040a4bdccb601974
kaisarrizky1-cyber <kaisarrizky1@gmail.com>
2026-06-04 22:14:42 +0800

    Update transform.js

--- api/transform.js
+++ api/transform.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
   const { prompt } = body;
 
   try {
-    const response = await fetch('https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0', {
+    const response = await fetch('https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5',{
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${HF_TOKEN}`,
